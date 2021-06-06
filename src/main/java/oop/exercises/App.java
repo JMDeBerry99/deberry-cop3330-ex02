f191280b131f73decfd6daeb87c1bb707082d211
Jacob DeBerry <JMDeBerry99@gmail.com>
2021-06-06 13:06:41 -0400

    Initial solution

--- src/main/java/oop/exercises/App.java
+++ src/main/java/oop/exercises/App.java
@@ -1,13 +1,20 @@
-package oop.exercises;
-
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Jacob DeBerry
  */
+package oop.exercises;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the input string? ");
+        String input = in.nextLine();
+        int count = input.length();
+
+        System.out.printf("%s has %d characters.", input, count);
     }
 }
